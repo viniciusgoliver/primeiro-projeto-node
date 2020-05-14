2763901fc04eb7c9384bce9dcab20a69ba8cd074
Vinícius G. Oliveira <vinicius.oliver@gmail.com>
2020-05-14 16:58:23 -0300

    Primeiro Commit

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
+export default routes;
